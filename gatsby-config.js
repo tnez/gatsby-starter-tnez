04c2aa20ff11d1593954e61b89123959d16dd642
Travis Nesland <tnesland@gmail.com>
2019-10-30 23:24:54 -0400

    chore: remove default starter boilerplate

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Starter`,
+    description: `Gatsby starter using Typescript and TailwindCSS`,
+    author: `@tnez`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-starter-tnez`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
